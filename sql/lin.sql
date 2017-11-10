b3814e9ef61bd924217970e38aef1bc25307216d
duwenchao <291871286@qq.com>
2017-11-10 18:42:53 +0800

    增加role_menu_relation表

--- sql/lin.sql
+++ sql/lin.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50528
 File Encoding         : 65001
 
-Date: 2017-10-26 14:23:48
+Date: 2017-11-10 18:42:05
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -24,7 +24,7 @@ CREATE TABLE `dept` (
   `num` int(11) DEFAULT NULL,
   `name` varchar(255) NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
 -- ----------------------------
 -- Records of dept
@@ -48,13 +48,42 @@ CREATE TABLE `login_log` (
   `message` varchar(255) DEFAULT NULL,
   `ip` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+) ENGINE=InnoDB AUTO_INCREMENT=100 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
 -- ----------------------------
 -- Records of login_log
 -- ----------------------------
-INSERT INTO `login_log` VALUES ('7', '登陆', '2', '2017-10-26 14:23:19', '成功', 'ttt', '0:0:0:0:0:0:0:1');
-INSERT INTO `login_log` VALUES ('8', '退出', '2', '2017-10-26 14:23:36', '成功', 'ttt', '0:0:0:0:0:0:0:1');
+INSERT INTO `login_log` VALUES ('87', '退出', '2', '2017-11-10 15:28:30', '成功', 'ttt', '0:0:0:0:0:0:0:1');
+INSERT INTO `login_log` VALUES ('88', '登陆失败', null, '2017-11-10 15:28:34', '成功', '用户名：admin,原因：账号与密码不匹配', '0:0:0:0:0:0:0:1');
+INSERT INTO `login_log` VALUES ('89', '登陆', '4', '2017-11-10 15:28:41', '成功', 'admin', '0:0:0:0:0:0:0:1');
+INSERT INTO `login_log` VALUES ('90', '登陆', '2', '2017-11-10 15:30:16', '成功', 'ttt', '0:0:0:0:0:0:0:1');
+INSERT INTO `login_log` VALUES ('91', '退出', '2', '2017-11-10 15:30:25', '成功', 'ttt', '0:0:0:0:0:0:0:1');
+INSERT INTO `login_log` VALUES ('92', '登陆', '4', '2017-11-10 15:30:31', '成功', 'admin', '0:0:0:0:0:0:0:1');
+INSERT INTO `login_log` VALUES ('93', '登陆', '2', '2017-11-10 15:32:10', '成功', 'ttt', '0:0:0:0:0:0:0:1');
+INSERT INTO `login_log` VALUES ('94', '退出', '2', '2017-11-10 15:32:13', '成功', 'ttt', '0:0:0:0:0:0:0:1');
+INSERT INTO `login_log` VALUES ('95', '登陆', '4', '2017-11-10 15:32:20', '成功', 'admin', '0:0:0:0:0:0:0:1');
+INSERT INTO `login_log` VALUES ('96', '登陆', '2', '2017-11-10 16:28:52', '成功', 'ttt', '0:0:0:0:0:0:0:1');
+INSERT INTO `login_log` VALUES ('97', '登陆', '2', '2017-11-10 16:38:18', '成功', 'ttt', '0:0:0:0:0:0:0:1');
+INSERT INTO `login_log` VALUES ('98', '登陆', '2', '2017-11-10 16:58:33', '成功', 'ttt', '127.0.0.1');
+INSERT INTO `login_log` VALUES ('99', '登陆', '2', '2017-11-10 17:32:11', '成功', 'ttt', '0:0:0:0:0:0:0:1');
+
+-- ----------------------------
+-- Table structure for memo
+-- ----------------------------
+DROP TABLE IF EXISTS `memo`;
+CREATE TABLE `memo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `text` varchar(255) DEFAULT NULL,
+  `time` varchar(255) DEFAULT NULL,
+  `userid` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of memo
+-- ----------------------------
+INSERT INTO `memo` VALUES ('14', 'admin', 'admin', '2017/11/9 上午10:29:25', '4');
 
 -- ----------------------------
 -- Table structure for menu
@@ -63,7 +92,7 @@ DROP TABLE IF EXISTS `menu`;
 CREATE TABLE `menu` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `code` varchar(255) DEFAULT NULL,
-  `pcode` varchar(255) DEFAULT NULL,
+  `parentid` int(255) DEFAULT NULL,
   `name` varchar(255) NOT NULL,
   `icon` varchar(255) DEFAULT NULL,
   `url` varchar(255) NOT NULL,
@@ -71,13 +100,18 @@ CREATE TABLE `menu` (
   `levels` int(11) DEFAULT NULL,
   `tips` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
 -- ----------------------------
 -- Records of menu
 -- ----------------------------
-INSERT INTO `menu` VALUES ('1', null, null, '用户管理', null, '/userpage', null, null, null);
-INSERT INTO `menu` VALUES ('2', null, null, '部门管理', null, '/deptpage', null, null, null);
+INSERT INTO `menu` VALUES ('1', null, null, '用户管理', 'fa-columns', '/userpage', null, '1', null);
+INSERT INTO `menu` VALUES ('2', null, null, '部门管理', 'fa-clone', '/deptpage', null, '1', null);
+INSERT INTO `menu` VALUES ('3', null, null, '发送通知', 'fa-share', '/noticepage', null, '1', null);
+INSERT INTO `menu` VALUES ('4', '', null, '我的便签', 'fa-sticky-note', '/memopage', null, '1', '');
+INSERT INTO `menu` VALUES ('5', null, null, '操作日志', 'fa-tags', '/operationlogpage', null, '2', null);
+INSERT INTO `menu` VALUES ('6', null, null, '登陆日志', 'fa-tags', '/loginlogpage', null, '2', null);
+INSERT INTO `menu` VALUES ('8', '', null, '角色管理', 'fa-tags', '/rolepage', null, '1', '');
 
 -- ----------------------------
 -- Table structure for notice
@@ -94,12 +128,13 @@ CREATE TABLE `notice` (
   `recivedeptid` int(11) DEFAULT NULL,
   `reciveuserid` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
 -- ----------------------------
 -- Records of notice
 -- ----------------------------
 INSERT INTO `notice` VALUES ('10', null, '欢迎使用Lin！                    (●\'◡\'●) ', null, '2', '系统通知', '2017-09-28 11:52:23', null, null);
+INSERT INTO `notice` VALUES ('11', null, 'hello', null, '2', '系统通知', '2017-11-10 09:40:42', null, null);
 
 -- ----------------------------
 -- Table structure for operation_log
@@ -116,11 +151,14 @@ CREATE TABLE `operation_log` (
   `state` varchar(255) DEFAULT NULL,
   `message` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+) ENGINE=InnoDB AUTO_INCREMENT=237 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
 -- ----------------------------
 -- Records of operation_log
 -- ----------------------------
+INSERT INTO `operation_log` VALUES ('234', '业务日志', '清除日志', '2', 'com.du.lin.controller.LogController', 'clearLoginLog', '2017-11-10 10:06:05', '成功', 'ttt');
+INSERT INTO `operation_log` VALUES ('235', '业务日志', '清除日志', '2', 'com.du.lin.controller.LogController', 'clearLoginLog', '2017-11-10 15:27:37', '成功', 'ttt');
+INSERT INTO `operation_log` VALUES ('236', '业务日志', '用户信息修改', '2', 'com.du.lin.controller.UserController', 'setuser', '2017-11-10 15:28:27', '成功', 'ttt');
 
 -- ----------------------------
 -- Table structure for role
@@ -131,13 +169,37 @@ CREATE TABLE `role` (
   `roles` varchar(255) NOT NULL,
   `tips` varchar(255) NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
 -- ----------------------------
 -- Records of role
 -- ----------------------------
 INSERT INTO `role` VALUES ('1', 'ROLE_ADMIN', '超级管理员');
 INSERT INTO `role` VALUES ('2', 'ROLE_USER', '用户');
+
+-- ----------------------------
+-- Table structure for role_menu_relation
+-- ----------------------------
+DROP TABLE IF EXISTS `role_menu_relation`;
+CREATE TABLE `role_menu_relation` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `roleid` int(11) NOT NULL,
+  `menuid` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of role_menu_relation
+-- ----------------------------
+INSERT INTO `role_menu_relation` VALUES ('1', '1', '1');
+INSERT INTO `role_menu_relation` VALUES ('2', '1', '2');
+INSERT INTO `role_menu_relation` VALUES ('3', '1', '3');
+INSERT INTO `role_menu_relation` VALUES ('4', '1', '4');
+INSERT INTO `role_menu_relation` VALUES ('5', '1', '5');
+INSERT INTO `role_menu_relation` VALUES ('6', '2', '4');
+INSERT INTO `role_menu_relation` VALUES ('7', '1', '6');
+INSERT INTO `role_menu_relation` VALUES ('8', '1', '7');
+INSERT INTO `role_menu_relation` VALUES ('9', '1', '8');
 
 -- ----------------------------
 -- Table structure for user
@@ -158,4 +220,4 @@ CREATE TABLE `user` (
 -- Records of user
 -- ----------------------------
 INSERT INTO `user` VALUES ('2', 'ttt', '9990775155c3518a0d7917f7780b24aa', '2', '1', '2', '1');
-INSERT INTO `user` VALUES ('4', 'admin', '21232f297a57a5a743894a0e4a801fc3', '4', '0', '3', '1');
+INSERT INTO `user` VALUES ('4', 'admin', '96e79218965eb72c92a549dd5a330112', '4', '0', '1', '2');
