8431222d72fbfe2a0018bf4deb7b9cc56b49cca7
duwenchao <291871286@qq.com>
2017-11-24 18:46:42 +0800

    src\main\java\generate\VelocityTest.java 通过此文件已经可以生成代码文件了

--- src/main/java/com/du/lin/controller/ThingController.java
+++ src/main/java/com/du/lin/controller/ThingController.java
@@ -10,7 +10,6 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
-
 @Controller
 @RequestMapping("/thing")
 public class ThingController {
@@ -28,10 +27,10 @@ public class ThingController {
 		String rows = request.getParameter("rows"); // 取得每页显示行数，,注意这是jqgrid自身的参数
 		return service.getShowJson(Integer.parseInt(page) , Integer.parseInt(rows));
 	}
+	
 	@ResponseBody
 	@RequestMapping(value = "operation" , method={RequestMethod.POST})
 	public String operationThing(HttpServletRequest request , Thing thing){
-		System.out.println(thing.toString());
 		boolean result = false;
 		String oper = request.getParameter("oper");
 		switch (oper) {
@@ -45,11 +44,7 @@ public class ThingController {
 			result = service.insert(thing);
 			break;
 		}
-		request.setAttribute("errorCode", "1111");
 		return "aaaa";
 	}
-	
 
-	
 }
-
